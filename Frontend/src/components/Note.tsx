bd123ddbe0d2be6b3c0f42e67c206c6406bf8f31
pararhythm <pararhythm.lab@gmail.com>
2026-09-02 20:53:34 +0700

    feat: Added the proof-of-concept Home theme

--- Frontend/src/components/Note.tsx
+++ Frontend/src/components/Note.tsx
@@ -4,10 +4,10 @@ interface NoteProps {
 }
 
 export default function Note({ title, content }: NoteProps) {
-  <div className="border-[#6D6D6D]">
-    <h2>{title}</h2>
-    <textarea name="content" id="content">
-      {content}
-    </textarea>
-  </div>;
+  return (
+    <div className="border border-[#6D6D6D] rounded-sm w-2xs h-56 p-1">
+      <h2 className="text-[#FFFFFF] font-bold text-2xl">{title}</h2>
+      <p className="text-[#FFFFFF] w-2xs h-50">{content}</p>
+    </div>
+  );
 }
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,9 +1,21 @@
 import Header from "../components/Header";
 
+import Note from "../components/Note";
+
+const Notes = [
+  { id: 1, title: "Recipes", content: "Pepper, meat, noodles" },
+  { id: 2, title: "Hello", content: "Hello world" },
+];
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col gap-12">
       <Header />
+      <div className="flex gap-5 items-center justify-start p-5">
+        {Notes.map((note) => (
+          <Note key={note.id} title={note.title} content={note.content} />
+        ))}
+      </div>
     </main>
   );
 }
